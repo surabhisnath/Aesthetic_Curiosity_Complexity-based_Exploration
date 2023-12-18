f642577b40548d96903a0c44892afdc1b8401284
Andrew Webb <andrew.webb@tuebingen.mpg.de>
2023-12-18 17:22:45 +0100

    Tidied up grid-search/MixedEffectsModelling

--- scripts/grid-search/MixedEffectsModelling.R
+++ scripts/grid-search/MixedEffectsModelling.R
@@ -70,8 +70,8 @@
   num_folds <- 3
 
   folds <- data %>%
-  group_by(Subject) %>%
-  modelr::crossv_kfold(k = num_folds)
+    group_by(Subject) %>%
+    modelr::crossv_kfold(k = num_folds)
 
   temp_train_1 <- folds$train$`1`
   data_train_fold1 <- data[temp_train_1$idx, ]
@@ -102,17 +102,17 @@ models <- list(
   "uInt + vInt + (1 | Subject)",
 
   "uLSC + uInt + vLSC + vInt + (1 | Subject)",
-  
+
   "uLSC * vLSC + (1 | Subject)",
   "uInt * vInt + (1 | Subject)",
   "uLSC * uInt + (1 | Subject)",
   "vLSC * vInt + (1 | Subject)",
-  
+
   "uLSC * vLSC + uInt * vInt + (1 | Subject)",  # best
   "uLSC * uInt + vLSC * vInt + (1 | Subject)",
   "uLSC + uInt + vLSC + vInt + uLSC:vInt + vLSC:uInt + (1 | Subject)",
   "uLSC + vLSC + uInt + vInt + uLSC:vLSC + uInt:vInt + uLSC:uInt + vLSC:vInt + uLSC:vInt + vLSC:uInt + (1 | Subject)",
-  
+
 
   # Supplementary analysis
 
@@ -123,37 +123,50 @@ models <- list(
   # quadratic effects
   "uLSCsq + uIntsq + vLSCsq + vIntsq + (1 | Subject)",
   "uLSCsq + vLSCsq + uIntsq + vIntsq + uLSC:vLSC + uInt:vInt + (1 | Subject)",
-  "uLSCsq + uIntsq + vLSCsq + vIntsq + uLSC:vLSC + uInt:vInt + (1 | Subject)", # L^2 + I^2
-  "(uLSCsq + vLSCsq + uLSC:vLSC) + (uIntsq + vIntsq + uInt:vInt) + (uLSC + vLSC):(uInt + vInt) + (1 | Subject)", # (L + I)^2
-  "uLSC + vLSC + uInt + vInt + uLSCsq + uIntsq + vLSCsq + vIntsq + (1 | Subject)", # L + I + L^2 + I^2 - interactions
-  "uLSC + vLSC + uInt + vInt + uLSCsq + uIntsq + vLSCsq + vIntsq + uLSC:vLSC + uInt:vInt + (1 | Subject)", # best = L + I + L^2 + I^2
+  "uLSCsq + uIntsq + vLSCsq + vIntsq + uLSC:vLSC + uInt:vInt + (1 | Subject)",
+  "(uLSCsq + vLSCsq + uLSC:vLSC) + (uIntsq + vIntsq + uInt:vInt) + (uLSC + vLSC):(uInt + vInt) + (1 | Subject)",
+  "uLSC + vLSC + uInt + vInt + uLSCsq + uIntsq + vLSCsq + vIntsq + (1 | Subject)",
+  "uLSC + vLSC + uInt + vInt + uLSCsq + uIntsq + vLSCsq + vIntsq + uLSC:vLSC + uInt:vInt + (1 | Subject)",
   "uLSC + vLSC + uInt + vInt + uLSCsq + uIntsq + vLSCsq + vIntsq + uLSC:vLSC + uInt:vInt + uLSC:uInt + vLSC:vInt + (1 | Subject)",
-  "uLSC + vLSC + uInt + vInt + uLSCsq + uIntsq + vLSCsq + vIntsq + uLSC:vLSC + uInt:vInt + uLSC:uInt + vLSC:vInt + uLSC:vInt + uInt:vLSC + (1 | Subject)" # L + I + (L + I)^2
+  "uLSC + vLSC + uInt + vInt + uLSCsq + uIntsq + vLSCsq + vIntsq + uLSC:vLSC + uInt:vInt + uLSC:uInt + vLSC:vInt + uLSC:vInt + uInt:vLSC + (1 | Subject)"
 )
 
 # Save all results to model_fits/Table_3_mixedeffects.csv
 {
-  df <- data.frame(matrix(ncol = 13, nrow = 0, dimnames =
-  list(NULL, c("Id", "model", "AIC", "BIC", "AIC/BIC Var",
-  "Rsq train mean", "Rsq train var", "Rsq test mean", "Rsq test var",
-  "RMSE train mean", "RMSE train var", "RMSE test mean", "RMSE test var"))))
+  df <-
+    data.frame(matrix(ncol = 13, nrow = 0,
+                      dimnames =
+                        list(NULL,
+                             c("Id", "model", "AIC", "BIC", "AIC/BIC Var",
+                               "Rsq train mean", "Rsq train var",
+                               "Rsq test mean", "Rsq test var",
+                               "RMSE train mean", "RMSE train var",
+                               "RMSE test mean", "RMSE test var"))))
 
   id <- 0
   for (formula in models) {
     id <- id + 1
     fullformula <- paste("num_clicks ~", formula)
-    f1 <- lmer(fullformula, data = data_train_fold1,
-    control = lmerControl(optimizer = "bobyqa"))
-    f2 <- lmer(fullformula, data = data_train_fold2,
-    control = lmerControl(optimizer = "bobyqa"))
-    f3 <- lmer(fullformula, data = data_train_fold3,
-    control = lmerControl(optimizer = "bobyqa"))
+    f1 <-
+      lmer(fullformula,
+           data = data_train_fold1,
+           control = lmerControl(optimizer = "bobyqa"))
+    f2 <-
+      lmer(fullformula, 
+           data = data_train_fold2,
+           control = lmerControl(optimizer = "bobyqa"))
+    f3 <- 
+      lmer(fullformula,
+           data = data_train_fold3,
+           control = lmerControl(optimizer = "bobyqa"))
 
     # Analyse model fit
-    metrics <- modelanalysis(num_folds,
-    list(f1, f2, f3), list(data_train_fold1, data_train_fold2,
-    data_train_fold3), list(data_test_fold1, data_test_fold2, data_test_fold3),
-    FALSE, FALSE, fullformula) # set second last param to TRUE for printing
+    metrics <-
+      modelanalysis(num_folds,
+                    list(f1, f2, f3),
+                    list(data_train_fold1, data_train_fold2, data_train_fold3),
+                    list(data_test_fold1, data_test_fold2, data_test_fold3),
+                    FALSE, FALSE, fullformula) # set second last param to TRUE for printing
     df[nrow(df) + 1, ] <- c(id, noquote(fullformula), metrics)
   }
 
@@ -166,8 +179,9 @@ models <- list(
 {
   bestformula <- "num_clicks ~ uLSC + vLSC + uInt + vInt + uLSC:vLSC + uInt:vInt + (1 | Subject)"
 
-  f <- lmer(bestformula, data = data,
-  control = lmerControl(optimizer = "nloptwrap"))
+  f <- lmer(bestformula,
+            data = data,
+            control = lmerControl(optimizer = "nloptwrap"))
 
   # make and save model vs predictions of train and test - Figure 3a,b
   make_plots(f, data_train_fold1, data_test_fold1)
@@ -184,40 +198,53 @@ models <- list(
   standard_errors <- sqrt(diag(vcov(f)))
   variable_names <- rownames(summary(f)$coefficients)
   p_values <- coef(summary(f))[, "Pr(>|t|)"]
-  signif_levels <- ifelse(p_values < 0.001, "***", ifelse(p_values < 0.01, "**", ifelse(p_values < 0.05, "*", ifelse(p_values < 0.1, ".", "NS"))))
-  results_df <- data.frame(Variable = variable_names, Coefficients = coefficients, StdError = standard_errors, PValue = p_values, Significance = signif_levels)
-  write.csv(results_df, file = "model_fits/Table_4_coeff_num_clicks.csv", row.names = FALSE)
+  signif_levels <-
+    ifelse(p_values < 0.001, "***",
+           ifelse(p_values < 0.01, "**",
+                  ifelse(p_values < 0.05, "*",
+                         ifelse(p_values < 0.1, ".", "NS"))))
+  results_df <-
+    data.frame(Variable = variable_names,
+               Coefficients = coefficients,
+               StdError = standard_errors,
+               PValue = p_values,
+               Significance = signif_levels)
+  write.csv(results_df,
+            file = "model_fits/Table_4_coeff_num_clicks.csv",
+            row.names = FALSE)
 }
 
 # plot interactions - Figure 3c,d
 {
   p <- interact_plot(f,
-      pred = vLSC, modx = uLSC, interval = TRUE,
-      x.label = "vLSC", y.label = "Number of Clicks",
-      legend.main = "uLSC", colors = "seagreen"
+    pred = vLSC, modx = uLSC, interval = TRUE,
+    x.label = "vLSC", y.label = "Number of Clicks",
+    legend.main = "uLSC", colors = "seagreen"
   ) + theme(
-      axis.title = element_text(family = "serif", size = 44),
-      axis.text = element_text(family = "serif", size = 26),
-      legend.text = element_text(family = "serif", size = 30),
-      legend.title = element_text(family = "serif", size = 40),
-      strip.text = element_text(family = "serif")
+    axis.title = element_text(family = "serif", size = 44),
+    axis.text = element_text(family = "serif", size = 26),
+    legend.text = element_text(family = "serif", size = 30),
+    legend.title = element_text(family = "serif", size = 40),
+    strip.text = element_text(family = "serif")
   )
 
   # Figure 3c
-  ggsave(filename = "plots/uLSC_vLSC_interaction.pdf", plot = p, width = 10, height = 10, units = "in")
+  ggsave(filename = "plots/uLSC_vLSC_interaction.pdf",
+         plot = p, width = 10, height = 10, units = "in")
 
   p <- interact_plot(f,
-      pred = vInt, modx = uInt, interval = TRUE,
-      x.label = "vInt", y.label = "Number of Clicks",
-      legend.main = "uInt", colors = "seagreen"
+    pred = vInt, modx = uInt, interval = TRUE,
+    x.label = "vInt", y.label = "Number of Clicks",
+    legend.main = "uInt", colors = "seagreen"
   ) + theme(
-      axis.title = element_text(family = "serif", size = 44),
-      axis.text = element_text(family = "serif", size = 26),
-      legend.text = element_text(family = "serif", size = 30),
-      legend.title = element_text(family = "serif", size = 44),
-      strip.text = element_text(family = "serif")
+    axis.title = element_text(family = "serif", size = 44),
+    axis.text = element_text(family = "serif", size = 26),
+    legend.text = element_text(family = "serif", size = 30),
+    legend.title = element_text(family = "serif", size = 44),
+    strip.text = element_text(family = "serif")
   )
 
   # Figure 3d
-  ggsave(filename = "plots/uInt_vInt_interaction.pdf", plot = p, width = 10, height = 10, units = "in")
+  ggsave(filename = "plots/uInt_vInt_interaction.pdf",
+         plot = p, width = 10, height = 10, units = "in")
 }
