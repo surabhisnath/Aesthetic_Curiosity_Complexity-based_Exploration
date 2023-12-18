d2eded3b563db31161e66b7eb7e696eb6d4204b4
Andrew Webb <andrew.webb@tuebingen.mpg.de>
2023-12-18 17:04:57 +0100

    Checked over complexity-extended/MixedEffectsModelling

--- scripts/complexity-extended/MixedEffectsModelling.R
+++ scripts/complexity-extended/MixedEffectsModelling.R
@@ -1,5 +1,6 @@
 ## Author: Surabhi S Nath
-## Description: This script implements mixed effects models of complexity ratings on 27x27, semi-revealed patterns.
+## Description: This script implements mixed effects models of complexity
+## ratings on 27x27, semi-revealed patterns.
 ## Helper functions and code for plotting in utils/Utils.R
 ## model analysis table written to model_fits/
 
@@ -22,7 +23,7 @@
 
   source("utils/Utils.R")
 
-  # Set seed 
+  # Set seed
   set.seed(20) # Seed set randomly for reproducibility
 }
 
@@ -30,30 +31,31 @@
 {
   # Read data
   data <- read.csv("utils/data.csv")
-  all_attention_failed = c('fafl6odp', 's7hnc00q', 'p60mbj11') # remove subjects who failed all attention checks
-  data <- data[! data$subject %in% all_attention_failed,]
+  # remove subjects who failed all attention checks
+  all_attention_failed <- c("fafl6odp", "s7hnc00q", "p60mbj11")
+  data <- data[! data$subject %in% all_attention_failed]
 
   # Scale all variables in the data
   my_scale <- function(x) {
     as.numeric(scale(x))
   }
 
-  data$rtime = data$rtime/1000
-  data$subject = factor(data$subject)
-  data$set = factor(data$set, levels = c(1,2,4))
-  data$pattern = factor(data$pattern)
-  data$num_colours = as.numeric(data$num_colours)
-  data$num_revealed = as.numeric(data$num_revealed)
-  data$visible_complexity_rating = my_scale(data$visible_complexity_rating)
-  data$imagined_complexity_rating = my_scale(data$imagined_complexity_rating)
-  data$LSC_sq = my_scale(data$LSC ^ 2)
-  data$intricacy_sq = my_scale(data$intricacy ^ 2)
-  data$underlying_LSC_sq = my_scale(data$underlying_LSC ^ 2)
-  data$underlying_intricacy_sq = my_scale(data$underlying_intricacy ^ 2)
-  data$LSC = my_scale(data$LSC)
-  data$intricacy = my_scale(data$intricacy)
-  data$underlying_LSC = my_scale(data$underlying_LSC)
-  data$underlying_intricacy = my_scale(data$underlying_intricacy)
+  data$rtime <- data$rtime / 1000
+  data$subject <- factor(data$subject)
+  data$set <- factor(data$set, levels = c(1, 2, 4))
+  data$pattern <- factor(data$pattern)
+  data$num_colours <- as.numeric(data$num_colours)
+  data$num_revealed <- as.numeric(data$num_revealed)
+  data$visible_complexity_rating <- my_scale(data$visible_complexity_rating)
+  data$imagined_complexity_rating <- my_scale(data$imagined_complexity_rating)
+  data$LSC_sq <- my_scale(data$LSC ^ 2)
+  data$intricacy_sq <- my_scale(data$intricacy ^ 2)
+  data$underlying_LSC_sq <- my_scale(data$underlying_LSC ^ 2)
+  data$underlying_intricacy_sq <- my_scale(data$underlying_intricacy ^ 2)
+  data$LSC <- my_scale(data$LSC)
+  data$intricacy <- my_scale(data$intricacy)
+  data$underlying_LSC <- my_scale(data$underlying_LSC)
+  data$underlying_intricacy <- my_scale(data$underlying_intricacy)
 }
 
 # K-fold stratified sampling
@@ -61,8 +63,8 @@
   num_folds <- 3
 
   folds <- data %>%
-  group_by(subject) %>%
-  modelr::crossv_kfold(k = num_folds)
+    group_by(subject) %>%
+    modelr::crossv_kfold(k = num_folds)
 
   temp_train_1 <- folds$train$`1`
   data_train_fold1 <- data[temp_train_1$idx, ]
@@ -105,27 +107,36 @@ models <- list(
 
 # Save all results to model_fits/
 {
-  df <- data.frame(matrix(ncol = 13, nrow = 0, dimnames =
-  list(NULL, c("Id", "model", "AIC", "BIC", "AIC/BIC Var",
-  "Rsq train mean", "Rsq train var", "Rsq test mean", "Rsq test var",
-  "RMSE train mean", "RMSE train var", "RMSE test mean", "RMSE test var"))))
+  df <- data.frame(matrix(ncol = 13, nrow = 0,
+                          dimnames =
+                            list(NULL,
+                                 c("Id", "model", "AIC", "BIC", "AIC/BIC Var",
+                                   "Rsq train mean", "Rsq train var",
+                                   "Rsq test mean", "Rsq test var",
+                                   "RMSE train mean", "RMSE train var",
+                                   "RMSE test mean", "RMSE test var"))))
 
   id <- 0
   for (formula in models) {
     id <- id + 1
     fullformula <- paste("visible_complexity_rating ~", formula)
-    f1 <- lmer(fullformula, data = data_train_fold1,
-    control = lmerControl(optimizer = "bobyqa"))
-    f2 <- lmer(fullformula, data = data_train_fold2,
-    control = lmerControl(optimizer = "bobyqa"))
-    f3 <- lmer(fullformula, data = data_train_fold3,
-    control = lmerControl(optimizer = "bobyqa"))
+    f1 <- lmer(fullformula,
+               data = data_train_fold1,
+               control = lmerControl(optimizer = "bobyqa"))
+    f2 <- lmer(fullformula,
+               data = data_train_fold2,
+               control = lmerControl(optimizer = "bobyqa"))
+    f3 <- lmer(fullformula,
+               data = data_train_fold3,
+               control = lmerControl(optimizer = "bobyqa"))
 
     # Analyse model fit
-    metrics <- modelanalysis(num_folds,
-    list(f1, f2, f3), list(data_train_fold1, data_train_fold2,
-    data_train_fold3), list(data_test_fold1, data_test_fold2, data_test_fold3),
-    FALSE, FALSE, fullformula) # set second last param to TRUE for printing
+    metrics <-
+      modelanalysis(num_folds,
+                    list(f1, f2, f3),
+                    list(data_train_fold1, data_train_fold2, data_train_fold3),
+                    list(data_test_fold1, data_test_fold2, data_test_fold3),
+                    FALSE, FALSE, fullformula) # set second last param to TRUE for printing
     df[nrow(df) + 1, ] <- c(id, noquote(fullformula), metrics)
   }
 
@@ -137,27 +148,31 @@ models <- list(
 # Plots saved to ./plots/
 {
   bestformula <- "visible_complexity_rating ~ LSC + intricacy + underlying_LSC + underlying_intricacy + ((LSC + underlying_LSC) | subject)"
-  f1 <- lmer(bestformula, data = data_train_fold1,
-  control = lmerControl(optimizer = "bobyqa"))
-  f2 <- lmer(bestformula, data = data_train_fold2,
-  control = lmerControl(optimizer = "bobyqa"))
-  f3 <- lmer(bestformula, data = data_train_fold3,
-  control = lmerControl(optimizer = "bobyqa"))
-  f <- lmer(bestformula, data = data_train_fold1,
-  control = lmerControl(optimizer = "bobyqa"))
+  f1 <- 
+    lmer(bestformula, data = data_train_fold1,
+         control = lmerControl(optimizer = "bobyqa"))
+  f2 <-
+    lmer(bestformula, data = data_train_fold2,
+         control = lmerControl(optimizer = "bobyqa"))
+  f3 <-
+    lmer(bestformula, data = data_train_fold3,
+         control = lmerControl(optimizer = "bobyqa"))
+  f <- 
+    lmer(bestformula, data = data_train_fold1,
+         control = lmerControl(optimizer = "bobyqa"))
 
   # Plots data vs predictions on train and test data
-  metrics <- modelanalysis(num_folds,
-  list(f1, f2, f3), list(data_train_fold1, data_train_fold2,
-  data_train_fold3), list(data_test_fold1, data_test_fold2, data_test_fold3),
-  FALSE, TRUE)
+  metrics <-
+    modelanalysis(num_folds,
+                  list(f1, f2, f3),
+                  list(data_train_fold1, data_train_fold2, data_train_fold3),
+                  list(data_test_fold1, data_test_fold2, data_test_fold3),
+                  FALSE, TRUE)
 
   # Plots random effects
   ggCaterpillar(ranef(f, condVar = TRUE))
   ggsave("plots/random_effects_complexity-extended.pdf")
-}
 
-# Save fixed effects to model_fits/
-{
+  # Save fixed effects to model_fits/
   write.csv(fixef(f), file = "model_fits/complexity-extended_coeff.csv")
 }
